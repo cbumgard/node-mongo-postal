d53627195d36c0e14f124a3b56f2ce4ae1ea1f6c
Chris Bumgardner <cbumgard@gmail.com>
2012-05-27 17:27:33 -0700

    cleanup, documentation

--- test/test-find-query.js
+++ test/test-find-query.js
@@ -1,17 +1,17 @@
 // Parse command-line options:
 var argv = require('optimist')
-    .usage('Usage: $0 -c [configFile] -z [zip] -r [radius] -m [max]')
-    .demand(['c'])
-    .alias('c', 'config')
-    .describe('c', 'Absolute path to a Javascript configuration file, e.g. /tmp/local.config.js')
-    .alias('z', 'zip')
-    .describe('z', 'Zipcode to search around')
-    .alias('r', 'radius')
-    .describe('r', 'Radius of the search')
-    .alias('m', 'max')
-    .describe('m', 'Maximum number of results to return')
-    .argv
-  ;
+  .usage('Usage: $0 -c [configFile] -z [zipcode] -r [radius] -m [max]')
+  .demand(['c'])
+  .alias('c', 'config')
+  .describe('c', 'Absolute path to a Javascript configuration file, e.g. /tmp/local.config.js')
+  .alias('z', 'zipcode')
+  .describe('z', 'Zipcode to search around')
+  .alias('r', 'radius')
+  .describe('r', 'Radius of the search')
+  .alias('m', 'max')
+  .describe('m', 'Maximum number of results to return')
+  .argv
+;
 
 // Initialize a connection to MongoDB:
 var mongoPostal = require('../lib/node-mongo-postal');
@@ -21,8 +21,10 @@ var collection = mongoPostal.initDb(config).collection(config.mongo.collection);
 
 var params = {
   dbCollection : collection,
-  zipcode : argv.zip ? argv.zip : "94102"
+  zipcode : argv.zipcode ? argv.zipcode : "94102"
 };
+if (argv.radius) params.radiusMiles = argv.radius;
+if (argv.max) params.maxResults = argv.max;
 
 mongoPostal.findPostals(params, function(err, postals) {
   if (err) {
@@ -32,7 +34,7 @@ mongoPostal.findPostals(params, function(err, postals) {
     postals.forEach(function(postal) {
       console.log("Found: %s", JSON.stringify(postal));
     });
-    console.log("Found %s postals", postals.length);
+    console.log("Found %d postals", postals.length);
     process.exit();
   }
 });
